8ee5505c09af0ca90343fd1c0df1aae69b9090f1
Tony <gth1123@naver.com>
2024-03-11 22:52:35 +0900

    함수

--- basic/04_입출력/4-1_function.py
+++ basic/04_입출력/4-1_function.py
@@ -61,10 +61,65 @@ def 함수이름(*매개변수):  # *args와 같이 사용
 '''
 
 ## 함수의 결과값은 언제나 하나이다.
-def add_and_mul(a, b):
-    return a + b, a * b  # return의 결과값은 튜플값 하나로 돌려준다.
-print(add_and_mul(3, 4))  # (7, 12)
+# def add_and_mul(a, b):
+#     return a + b, a * b  # return의 결과값은 튜플값 하나로 돌려준다.
+# print(add_and_mul(3, 4))  # (7, 12)
 
 # return은 함수를 빠져나갈 때에도 사용할 수 있다.
 
 ## 매개변수에 초깃값 미리 설정하기
+# def say_myself(name, age, man=True):
+#     print("나의 이름은 %s입니다." % name)
+#     print("나이는 %d살입니다." % age)
+#     if man:
+#         print("남자입니다.")
+#     else:
+#         print("여자입니다.")
+
+# say_myself("박응용", 27)  # 남자입니다.
+# say_myself("박응용", 27, True)  # 남자입니다.
+# say_myself("박응선", 27, False)  # 여자입니다.
+# say_myself("박응용") # TypeError: say_myself() missing 2 required positional arguments: 'age' and
+
+# 함수 안에서 선언한 변수의 효력 범위
+# a = 1
+# def vartest(a):
+#     a = a + 1  # 함수 안에서 사용하는 변수는 함수 밖의 변수와는 전혀 상관이 없다.
+
+# vartest(a)
+# print(a)  # 1
+
+# 함수 안에서 함수 밖의 변수를 변경하는 방법
+# 1. return 사용하기
+# a = 1
+# def vartest(a):
+#     a = a + 1
+#     return a
+  
+# a = vartest(a)
+# print(a)  # 2
+
+# 2. global 명령어 사용하기 - global 명령어는 사용하지 않는 것이 좋다.
+# a = 1
+# def vartest():
+#     global a
+#     a = a + 1
+
+# vartest()
+# print(a)  # 2
+
+# ------------------------------
+
+# lambda 예약어
+# lambda는 함수를 생성할 때 사용하는 예약어로 def와 동일한 역할을 한다.
+# 보통 함수를 한줄로 간결하게 만들 때 사용한다.
+# def를 사용해야 할 정도로 복잡하지 않거나 def를 사용할 수 없는 곳에 주로 쓰인다.
+
+'''
+# 사용법
+함수_이름 = lambda 매개변수1, 매개변수2, ...: 매개변수를 이용한 표현식
+'''
+add = lambda a, b: a + b
+result = add(3, 4)
+print(result)  # 7
+
